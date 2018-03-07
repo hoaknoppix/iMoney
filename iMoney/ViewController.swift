2290ca545f0783b434479a25a194e529391c0829
Hoa Tran <hoaknoppix@gmail.com>
2018-03-07 19:31:31 +0700

    First commit

---
+++ iMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMoney
+//
+//  Created by hoaqt on 3/6/18.
+//  Copyright © 2018 com.noron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
